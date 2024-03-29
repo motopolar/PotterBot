fc2643513a89036e2100c52198e735ff3cddf288
motopolar <120253012+motopolar@users.noreply.github.com>
2024-03-29 03:42:10 -0700

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,9 +1,12 @@
+let fs = require('fs')
+
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
-if (text) {
-global.db.data.chats[m.chat].sWelcome = text
-conn.reply(m.chat, lenguajeGB.smsSetW(), fkontak, m)
-//conn.sendButton(m.chat, wm, lenguajeGB['smsSetW'](), null, [[lenguajeGB.smsConMenu(), `/menu`]], fkontak, m)
-} else throw `${lenguajeGB['smsSetW2']()}`
+    if (fs.existsSync('media/pictures/welcome.jpg')) {
+        global.db.data.chats[m.chat].sWelcome = 'media/pictures/welcome.jpg'
+        conn.reply(m.chat, lenguajeGB.smsSetW(), fkontak, m)
+    } else {
+        throw `${lenguajeGB['smsSetW2']()}`
+    }
 }
 handler.command = ['setwelcome', 'bienvenida'] 
 handler.botAdmin = true
