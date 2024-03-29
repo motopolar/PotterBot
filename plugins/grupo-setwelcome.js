37d01a9f2c3e9275883e477b0d905da1c7beb545
motopolar <120253012+motopolar@users.noreply.github.com>
2024-03-29 04:01:08 -0700

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,15 +1,18 @@
 let fs = require('fs')
 
-let handler = async (m, { conn }) => {
-    if (fs.existsSync('media/menu/welcome.jpg')) {
-        let imageBuffer = fs.readFileSync('media/menu/welcome.jpg')
-        conn.sendMessage(m.chat, imageBuffer, 'imageMessage', { quoted: m })
+let handler = async (m, { conn, text }) => {
+    if (text) {
+        global.db.data.chats[m.chat].sWelcome = text
+        conn.reply(m.chat, '🤩🤩🤩🤩🤩🤩 ¡Entendido! Daré esta bienvenida a todos los nuevos miembros.', null, m)
+    } else if (fs.existsSync('media/menu/welcome.jpg')) {
+        global.db.data.chats[m.chat].sWelcome = 'media/menu/welcome.jpg'
+        conn.reply(m.chat, lenguajeGB.smsSetW(), null, m)
     } else {
         throw `${lenguajeGB['smsSetW2']()}`
     }
 }
+handler.help = ['setwelcome <texto>']
+handler.tags = ['group']
 handler.command = ['setwelcome', 'bienvenida'] 
-handler.botAdmin = true
 handler.admin = true
-handler.group = true
 export default handler
